46549c810976cf747b9e15ac2df988b91b2476b1
SJLimburg <forestgirl6@hotmail.com>
2020-09-26 17:12:49 -0400

    Partially working PyBank - need list aveage and to write to txt file

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,48 +12,49 @@ with open(budget_data, 'r') as csv_file:
     csv_monthly = csv.reader(csv_file, delimiter=",")
     csv_header = next(csv_file)
 
-    print(f"Header: {csv_header}")
-
     Months = 0
     TotalProfit = 0
-    AvgMonChange = 0
+    RevChange = 0
+    ProfitChangeList = []
+    PrevRev = 0
+    GreatestGain = ["",0] 
+    GreatestLoss = ["", -100]
+    
 
     for rows in csv_monthly:
+        # Count months and derive total profit
+        Months = Months+1
         TotalProfit = TotalProfit  + int(rows[1])
-       
+        #Calculate  revenue change for the month
+        RevChange = int(rows[1]) - PrevRev
+        PrevRev = int(rows[1])
+        #Save the revenue change into a list
+        ProfitChangeList.append(RevChange)
+        
+        #Compare the current month revenue to the previous GreatestGain & save if greater
+        if (RevChange > GreatestGain[1]):
+            GreatestGain[0]=rows[0]
+            GreatestGain[1]=RevChange
+
+        #Compare the current month revenue to the previous GreatestLoss & save if worse
+        if (RevChange < GreatestLoss[1]):
+            GreatestLoss[0]=rows[0]
+            GreatestLoss[1]=RevChange
+
+# print(*ProfitChangeList, sep =", ")
+# avgMonChg = sum(ProfitChangeList)/len(ProfitChangeList)
+# print(avgMonChg)
+
+# checking output for eventual writing into output file
+print("Financial Analysis \n--------------------------------\n")
+print(f"Total Months:  {Months}\n" f"Total Revenue:   ${TotalProfit}\n")
+print(f'Greatest Increase in Profits: {GreatestGain[0]} (${GreatestGain[1]})')
+print(f'Greatest Decrease in Profits: {GreatestLoss[0]} (${GreatestLoss[1]})')
 
 
-#  Inside loop have a Variable Months to increment every loop to derive total months under report.
-# Also loop through
-#  MonthlyChange =  (curMonPL - prevMonPl ) to get difference ;
-#  TotalProfit = TotalProfit  + Col2
-#  TotalMonthyChange =  TotalMonthyChange + MonthlyChange:
-# Average Monthly Change = TotalMonthyChange/Months
-# conditional loop:
-# Set GreatestGain to  first MonthlyChange, Store amount and month - go to next and compare
-# set GreatestLoss to the first MonthlyChange   Store amount and month - go to next and compare  - Beware of subtraction differences.   
 
-# The total number of months included in the dataset - file name budget_data.csv
-# The net total amount of "Profit/Losses" over the entire period - Get the sum of all months p/l
-# The average of the changes in "Profit/Losses" over the entire period - get the mean of p/l
-# The greatest increase in profits (date and amount) over the entire period - MOM calculation Mon2-mon1 etc = monthly change
-# The greatest decrease in losses (date and amount) over the entire period - MOM calculation - greatest MOM negative
-# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
-
-#Total months
-# total net profit/loss
-print(TotalProfit)
-# average monthyly change in profit
-# greatest monthly increase amount and month
-# greatest monthly decrease amount and month
-
-# # Import necessary dependencies
-# import csv
-# # Create the path for the filename
-# data_output = os.path.join("Analysis", "PyBank.txt")
-# # Write data to a .csv file
-# with open(data_output, "w", newline="") as csvfile:
-#  writer = csv.writer(csvfile)
+# with open(data_output, "w", newline="") as txt_file:
+#  writer = txt.writer(txt_file)
 #  # To save specific data input as a row in the csv
 #  writer.writerow(["row1", "row2"])
 
