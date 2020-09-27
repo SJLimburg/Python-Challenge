f12e148fb1d442847d77e8d279b2063931bb094a
SJLimburg <forestgirl6@hotmail.com>
2020-09-27 01:14:49 -0400

    PyBank Final

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,7 +4,8 @@ import os
 import csv
 # set path for csv file as 
 budget_data = os.path.join( "Resources", "budget_data.csv")
-# C:\Users\Shirley\Desktop\Homework_BC_SJL\03-Homeworrk_Python\Python-Challenge\PyBank
+#set up path for saving output to text file
+output_data = os.path.join("Analysis","Financial_Analysis.txt")
 
 
 # read the Csv file and set up a loop to count rows
@@ -12,24 +13,30 @@ with open(budget_data, 'r') as csv_file:
     csv_monthly = csv.reader(csv_file, delimiter=",")
     csv_header = next(csv_file)
 
+    #Initialize Variables
     Months = 0
     TotalProfit = 0
     RevChange = 0
-    ProfitChangeList = []
     PrevRev = 0
+
+    #Set up Lists
+    ProfitChangeList = []
     GreatestGain = ["",0] 
     GreatestLoss = ["",0]
     
 
     for rows in csv_monthly:
         # Count months and derive total profit
-        Months = Months+1
-        TotalProfit = TotalProfit  + int(rows[1])
-        #Calculate  revenue change for the month
+        Months = Months + 1
+        TotalProfit = TotalProfit + int(rows[1])
+        
+        #Calculate  revenue change for the month    
         RevChange = int(rows[1]) - PrevRev
-        PrevRev = int(rows[1])
+        PrevRev = int(rows[1])    
+        
         #Save the revenue change into a list
-        ProfitChangeList.append(RevChange)
+        if Months > 1:
+            ProfitChangeList.append(RevChange)
         
         #Compare the current month revenue to the previous GreatestGain & save if greater
         if (RevChange > GreatestGain[1]):
@@ -44,20 +51,23 @@ with open(budget_data, 'r') as csv_file:
 # print(*ProfitChangeList, sep =", ")
 sumMonChg =sum(ProfitChangeList)
 print(f'sum of monthly change {sumMonChg}')
-avgMonChg = sum(ProfitChangeList)/len(ProfitChangeList)
+avgMonChg = round(sum(ProfitChangeList)/len(ProfitChangeList),2)
 
 
-# checking output for eventual writing into output file
-print("Financial Analysis \n--------------------------------\n")
-print(f"Total Months:  {Months}\n" f"Total Revenue:   ${TotalProfit}\n")
-print(f'Average Change: {avgMonChg}')
+# Print output for to terminal
+print("Financial Analysis \n--------------------------------")
+print(f"Total Months:  {Months}\n" f"Total Revenue:   ${TotalProfit}")
+print(f'Average Change: ${avgMonChg}')
 print(f'Greatest Increase in Profits: {GreatestGain[0]} (${GreatestGain[1]})')
 print(f'Greatest Decrease in Profits: {GreatestLoss[0]} (${GreatestLoss[1]})')
 
 
+with open(output_data, 'w') as txt_file:
+    txt_file.write("Financial Analysis \n--------------------------------\n")
+    txt_file.write(f"Total Months:    {Months}\n")
+    txt_file.write(f"Total Revenue:   ${TotalProfit}\n")
+    txt_file.write(f'Average Change: ${avgMonChg}\n')
+    txt_file.write(f'Greatest Increase in Profits: {GreatestGain[0]} (${GreatestGain[1]})\n')
+    txt_file.write(f'Greatest Decrease in Profits: {GreatestLoss[0]} (${GreatestLoss[1]})\n')
 
-# with open(data_output, "w", newline="") as txt_file:
-#  writer = txt.writer(txt_file)
-#  # To save specific data input as a row in the csv
-#  writer.writerow(["row1", "row2"])
 
