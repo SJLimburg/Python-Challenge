65611e137890e598b27191b3b11d18542e9f7522
SJLimburg <forestgirl6@hotmail.com>
2020-09-27 15:56:48 -0400

    Pypoll working copy

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,7 +3,7 @@ import os
 import csv
 
 # set up paths
-poll_data = os.path.join("Resources","election_data.csv")
+poll_data = os.path.join("Resources","election_data_test.csv")
 results_file = os.path.join("Analysis","Election_Results.txt")
 
 # read csv file
@@ -32,8 +32,30 @@ with open(poll_data, 'r') as vote_file:
             votes[candidate_index] = votes[candidate_index] + 1
         else:
             candidate_list.append(candidate_name)
-            votes.append(1)
-            temp = len([votes])
+            votes.append(1)  
+            
+candidate_count = len([candidate_list])
+
+with open(results_file, 'w') as txt_file:
+    txt_file.write(f"Election Results\n ---------------------------------\n")
+    txt_file.write(f"Total Votes:   {overall_vote_count}\n---------------------------------\n")
+    write_index = 0
+    winner_index = 0
+    winner = candidate_list[0]
+    for items in candidate_list:
+        name = candidate_list[write_index]
+        percent = votes[write_index]/overall_vote_count
+        votes_for_candidate = votes[write_index]
+        txt_file.write(f"{name}:  {percent}%  ({votes_for_candidate})\n")
+        
+        
+        if votes[write_index] > votes[winner_index]:
+            winner_index = write_index
+            winner = candidate_list[winner_index]
+       
+        write_index = write_index + 1    
+    txt_file.write(f"---------------------------------\n Winner: {winner}\n---------------------------------\n")
+
 
            
            
